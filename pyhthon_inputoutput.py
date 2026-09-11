624fdec280ebf50b864c6fdaea2be25468a16d1d
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-11 15:38:56 +0530

    Add files via upload

---
+++ pyhthon_inputoutput.py
@@ -0,0 +1,15 @@
+# num1= input('please enter the first  number:')
+# num2= input('please enter the second number:')
+
+# print(num1+num2)
+# print(str(num1)+str(num2))
+
+# print(int(num1)+int(num2))
+# print(type(num1))
+
+#NOW SAME DO FOR STRINGS
+str1= input('please enter first name:')
+str2= input('please enter second name:')
+str3= input('please enter third name:')
+
+print('hello'+' '+ str1+ ''+ str2+str3)
