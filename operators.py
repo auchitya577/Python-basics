616a4388ef7988fd2ed1fe9e262b130fcd563e25
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-12 09:46:59 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,15 @@
+# a= True
+# b= False
+
+# if a or b:
+#     print("ALL TRUEE!!")
+
+a= True
+b= False
+
+if not(a) or not(b):
+    print("ALL TRUEE!!")
+
+# THERE ARE DIFFERENT OPERATORS LIKE
+
+# OR, AND, NOT, ADD,SUB,DIVIDE,MULTI
