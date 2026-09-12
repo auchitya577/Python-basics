9077eda2926f82521c267723f9bf9f36a31c7b06
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-12 09:46:46 +0530

    Add files via upload

---
+++ stringshellowrold.py
@@ -0,0 +1,14 @@
+# a = "this is a single line"
+
+# print(a)
+
+# b= "this is a multi"\
+# "line of streing"
+# print(b)
+
+
+# a= "hello"
+# b="auchit how are u"
+# print(a+b)
+
+print('hello','there', 'ia am using whatsapp',sep=',')
