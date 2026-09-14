61256650b8f53b0ac0d6771406dcabbd0b13bf02
Auchitya Singh Rajput <auchityasinghrajput@gmail.com>
2026-09-14 21:38:37 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,18 @@
+# x ="10.8"
+# y=float(x)
+# print(y)
+
+# THE TYPE IS USED FOR CONVERTING THE DATA TYPE LIKE STRING TO INT ETC.
+# x=25
+# y=2.5
+
+# z=x+y
+# print(z)
+# print(type(z))
+
+
+x="10.85"
+y=str(x)
+
+print(y)
+print(type(y))
